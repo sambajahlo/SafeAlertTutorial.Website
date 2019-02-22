5955709c253be417c947ff07cd9a8b08aadf4b11
Samba Diallo <shabashiki@gmail.com>
2019-02-22 09:38:15 -0800

    inital commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const pug = require('pug');
+const app = express()
+
+app.set('view engine', 'pug')
+//app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('/',(req,res) =>{
+  res.render('index',
+  {
+    lat: 37.33182,
+    lon: -122.03118,
+    uuid: "uuid"
+   })
+})
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
